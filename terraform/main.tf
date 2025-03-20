dc19e7c2733b5b186c5d066ede9273400e02b9f5
aditya guleria <addy@adityas-MacBook-Air.local>
2025-03-20 17:14:57 -0400

    Ansible Config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
-resource "aws_instance" "geth_node" {
+resource "aws_instance" "geth_node_test" {
   ami           = "ami-04b4f1a9cf54c11d0" # Ubuntu 22.04 (Free Tier)
   instance_type = var.instance_type
   key_name      = var.key_name
 
 
   tags = {
-    Name = "GethNode1"
+    Name = "GethNode"
   }
 }
 
